9f0e96bebfee1bb1e94352398b4462b43b4da04b
shielarlamb <aleisharizly_soliuga@yahoo.com>
2018-01-02 07:45:17 -0600

    update

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ $(() => {
 
 			data.employment.forEach((jobs) => {
 				$("#employment").append(`<ul><li><strong>${jobs.position}</strong></li><li>${jobs.company}</li><li>${jobs.address}</li><li>${jobs.date}</li><p>${jobs.description}</p><p><strong>Key Accomplishment/s: </strong>${jobs.accomplishment}</p></ul>`);
-
+				
 			});
 		};
 
